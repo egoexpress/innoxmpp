b3a58a165fc4f0cd428aa540a296bb744b443991
Bjoern Stierand <bjoern-github@innovention.de>
2012-09-09 21:41:19 +0200

    added scheduling for 'check free space' function (needs implementation though)

--- bots/LinuxBot.py
+++ bots/LinuxBot.py
@@ -13,11 +13,29 @@ class LinuxBot(GenericBot):
     LinuxBot - the Linux-related bot
     """
 
+    # initialize class
     def __init__(self):
         """
         Designated initializer
         """
         super(LinuxBot,self).__init__()
+
+    def _scheduleTasks(self):
+        """
+        add tasks to be scheduled to global scheduler
+
+        The scheduler needs a callback to be called after the given interval
+        has passed. As it accepts no class functions as callback, we create a
+        wrapper using a local function which calls the class function
+        """
+
+        super(LinuxBot,self)._scheduleTasks()
+
+        # schedule checking of free space
+        def checkFreeSpace():
+            self.taskCheckFreeSpace()
+
+        self.schedule("Check Free Space", 60, checkFreeSpace, repeat=True)
 
     # handler for the Linux 'uptime' command
     def handleUptimeCommand(self, _sender, _arguments):
@@ -30,3 +48,15 @@ class LinuxBot(GenericBot):
         returnCode, result = self.executeShellCommand("echo $HOSTNAME && uptime")
         if returnCode == 0:
             self.sendMessage(_sender, result)
+
+    # callback to check system free space
+    def taskCheckFreeSpace(self):
+        """
+        Check free disk space and send warning to registered users
+        """
+        self.logger.debug("Performing task taskCheckFreeSpace")
+
+        # TODO: implement logic here
+
+        # for jid in self.targetJIDs:
+            # self.sendMessage(jid, "Test Message")
