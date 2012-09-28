029d33f60c0c31bf562cdeade6f329d9d2620ea7
Bjoern Stierand <bjoern-github@innovention.de>
2012-09-28 19:27:11 +0200

    initial addition of BotRunner class

---
+++ framework/BotRunner.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+    BotRunner - a running class for innoxmpp bots
+    Part of the InnoXMPP framework
+    Copyright (C) 2012 Bjoern Stierand
+"""
+
+import bots
+
+class BotRunner():
+	"""
+	BotRunner - execute bots and handle command line input
+	"""
+
+	def __init__(self, botclass):
+		"""
+		Designated initializer
+		"""
+		pass
+
+
